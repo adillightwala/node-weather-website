49368d462433f9d5593135e6147c007511ce5130
adillightwala <adillightwala20.3@gmail.com>
2021-09-14 11:19:34 +0530

    Init commit

---
+++ src/app.js
@@ -0,0 +1,106 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const forcast = require("./utils/forecast");
+const grocode = require("./utils/geocode");
+const geocode = require("./utils/geocode");
+const { error } = require("console");
+
+const app = express();
+
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup Handlebar engines and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Setup statuc directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Adil Lightwala",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Adil Lightwala",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text.",
+    title: "Help",
+    name: "Adil Lightwala",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
+    });
+  } else {
+    geocode(
+      req.query.address,
+      (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+          return res.send({
+            error,
+          });
+        }
+        forcast(latitude, longitude, (error, forcastData) => {
+          if (error) {
+            return res.send({
+              error,
+            });
+          }
+          res.send({
+            forcast: forcastData,
+            location,
+            address: req.query.address,
+          });
+        });
+      }
+    );
+  }
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+  res.send({
+    products: [],
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Adil Lightwala",
+    errorMessage: "Help article not found!",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Adil Lightwala",
+    errorMessage: "Page Note Found",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000.");
+});
